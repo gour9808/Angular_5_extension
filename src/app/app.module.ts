ea026a2b2d8fdc14f51caf87153e9da65814f132
gour98808 <gouravsharma614@gmail.com>
2018-06-18 11:46:18 +0530

    added modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule, Http } from '@angular/http';
+import { DataTableModule, SharedModule } from 'primeng/primeng';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routes';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ContainerComponent } from './container/container.component';
+import { ChartModule } from 'primeng/primeng';
+import { HttpClientModule } from '@angular/common/http';
+import { TableModule } from 'primeng/table';
+import { BooksService } from './services/books.service';
+import { ListItemComponent } from './list-item/list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ToolbarComponent,
+    ContainerComponent, ListItemComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule,
+    HttpModule, ChartModule,
+    DataTableModule,
+    SharedModule, TableModule,
+    FlexLayoutModule,
+    HttpClientModule,
+
   ],
-  providers: [],
+  providers: [
+    BooksService],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
