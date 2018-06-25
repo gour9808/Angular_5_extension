f29eb2e572c23e89fc8614d30dbfcd8db5a0d200
gour98808 <gouravsharma614@gmail.com>
2018-06-25 16:20:58 +0530

    added interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, Http } from '@angular/http';
 import { DataTableModule, SharedModule } from 'primeng/primeng';
+import { ToastyModule } from 'ng2-toasty';
+import { SlimLoadingBarModule, SlimLoadingBarService } from 'ng2-slim-loading-bar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routes';
@@ -16,13 +18,19 @@ import { TableModule } from 'primeng/table';
 import { BooksService } from './services/books.service';
 import { ListItemComponent } from './list-item/list-item.component';
 import { ToastMessageService } from './services/toast-message.service';
+import { ToolbarTitleService } from './services/toolbar-title.service';
+import { CookieService } from './services/cookie.service';
+import { AuthService } from './services/auth.service';
+import { SplashLoaderComponent } from './spalsh-loader/spalsh-loader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     ToolbarComponent,
-    ContainerComponent, ListItemComponent,
+    ContainerComponent,
+    ListItemComponent,
+    SplashLoaderComponent,
   ],
   imports: [
     BrowserAnimationsModule,
@@ -34,9 +42,16 @@ import { ToastMessageService } from './services/toast-message.service';
     SharedModule, TableModule,
     FlexLayoutModule,
     HttpClientModule,
+    ToastyModule.forRoot(),
+    SlimLoadingBarModule.forRoot(),
 
   ],
-  providers: [ToastMessageService,
+  providers: [
+    ToastMessageService,
+    SlimLoadingBarService,
+    ToolbarTitleService,
+    CookieService,
+    AuthService,
     BooksService],
 
   bootstrap: [AppComponent]
